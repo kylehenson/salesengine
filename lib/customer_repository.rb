424a1a0dd665175d144838ea89f1e2ca1cfc02d7
Kyle Henson <khenson.kh@gmail.com>
2015-03-12 15:14:58 -0600

    Made Customer Repo

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -9,4 +9,61 @@ class CustomerRepository
     @customers = parser.parse
   end
 
+  def all
+    customers
+  end
+
+  def random
+    customers.sample
+  end
+
+  def find_by_id(id)
+    find_by_attribute(:id, id)
+  end
+
+  def find_by_first_name(first_name)
+    find_by_attribute(:first_name, first_name)
+  end
+
+  def find_by_last_name(last_name)
+    find_by_attribute(:last_name, last_name)
+  end
+
+  def find_by_created_at(created_at)
+    find_by_attribute(:created_at, created_at)
+  end
+
+  def find_by_updated_at(updated_at)
+    find_by_attribute(:updated_at, updated_at)
+  end
+
+  def find_all_by_id(id)
+    find_all_by_attribute(:id, id)
+  end
+
+  def find_all_by_first_name(first_name)
+    find_all_by_attribute(:first_name, first_name)
+  end
+
+  def find_all_by_last_name(last_name)
+    find_all_by_attribute(:last_name, last_name)
+  end
+
+  def find_all_by_created_at(created_at)
+    find_all_by_attribute(:created_at, created_at)
+  end
+
+  def find_all_by_updated_at(updated_at)
+    find_all_by_attribute(:updated_at, updated_at)
+  end
+
+  private
+
+  def find_by_attribute(attribute, given)
+    customers.detect { |customer| customer.send(attribute) == given }
+  end
+
+  def find_all_by_attribute(attribute, given)
+    customers.select { |customer| customer.send(attribute) == given }
+  end
 end
--- test/customer_repository_test.rb
+++ test/customer_repository_test.rb
@@ -7,15 +7,84 @@ class CustomerRepositoryTest < Minitest::Test
     assert CustomerRepository
   end
 
-  def test_it_holds_six_customer_instances
+  def test_it_holds_seven_customer_instances
     customer_repo = CustomerRepository.new('./test/support/customers.csv')
-    assert customer_repo.customers.count == 6
+    assert customer_repo.customers.count == 7
   end
 
   def test_it_contains_parsed_customer_objects
     customer_repo = CustomerRepository.new('./test/support/customers.csv')
     first = customer_repo.customers.first
     assert_equal "Joey", first.first_name
+  end
+
+  def test_it_can_return_all_customers
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    all = customer_repo.all
+    assert_equal 7, all.count
+    assert_equal Customer, all[0].class
+  end
+
+  def test_it_can_return_a_random_customer
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    random =  customer_repo.random
+    assert random
+    assert_equal Customer, random.class
+  end
+
+  def test_it_can_find_an_instance_of_customer_by_id
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    assert_equal "Mariah", customer_repo.find_by_id(3).first_name
+  end
+
+  def test_it_can_find_an_instance_of_customer_by_first_name
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    assert_equal 2, customer_repo.find_by_first_name("Cecelia").id
+  end
+
+  def test_it_can_find_an_instance_of_customer_by_last_name
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    assert_equal 3, customer_repo.find_by_last_name("Toy").id
+  end
+
+  def test_it_can_find_an_instance_of_customer_by_created_at
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    assert_equal 2, customer_repo.find_by_created_at("2012-03-27 14:54:10 UTC").id
+  end
+
+  def test_it_can_find_an_instance_of_customer_by_updated_at
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    assert_equal 2, customer_repo.find_by_updated_at("2012-03-27 14:54:10 UTC").id
+  end
+
+  def test_it_can_find_all_instances_of_customer_by_id
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    assert_equal 2, customer_repo.find_all_by_id(1).count
+  end
+
+  def test_it_can_find_all_instances_of_customer_by_first_name
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    assert_equal 2, customer_repo.find_all_by_first_name("Joey").count
+  end
+
+  def test_it_can_find_all_instances_of_customer_by_last_name
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    assert_equal 2, customer_repo.find_all_by_last_name("Ondricka").count
+  end
+
+  def test_it_can_find_all_instances_of_customer_by_created_at
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    assert_equal 2, customer_repo.find_all_by_created_at("2012-03-27 14:54:09 UTC").count
+  end
+
+  def test_it_can_find_all_instances_of_customer_by_updated_at
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')
+    assert_equal 2, customer_repo.find_all_by_updated_at("2012-03-27 14:54:09 UTC").count
+  end
+
+  def test_it_returns_an_empty_array_if_no_matches_for_find_all_by_x
+    customer_repo = CustomerRepository.new('./test/support/customers.csv')    
+    assert_equal [], customer_repo.find_all_by_id(10)
   end
 
 end
